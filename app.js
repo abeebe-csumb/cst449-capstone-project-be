d0c66113cf6f5e4a7aa34e15adbba4ec23698a2c
abeebe-csumb <abeebe@csumb.edu>
2021-10-25 17:25:35 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+//routes
+app.get('/', (req, res) => {
+    res.send('hello world');
+  }); //test
+
+//listen
+app.listen(3000, () => {
+    console.log('server started');
+});
