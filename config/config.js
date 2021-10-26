07f1177877807a2e569d936186de13bd5850da18
abeebe-csumb <abeebe@csumb.edu>
2021-10-26 16:24:35 -0700

    update env variables

--- config/config.js
+++ config/config.js
@@ -2,7 +2,21 @@ require("dotenv").config();
 
 var config_vars =
     {
-        "database": {
+        "development": {
+            "username": process.env.DB_SERVER_USER,
+            "password": process.env.DB_SERVER_PASSWORD,
+            "database": process.env.DB_SERVER_DATABASE,
+            "host": process.env.DB_SERVER_HOST,
+            "dialect": "mysql"
+          },
+          "test": {
+            "username": process.env.DB_SERVER_USER,
+            "password": process.env.DB_SERVER_PASSWORD,
+            "database": process.env.DB_SERVER_DATABASE,
+            "host": process.env.DB_SERVER_HOST,
+            "dialect": "mysql"
+          },
+          "production": {
             "username": process.env.DB_SERVER_USER,
             "password": process.env.DB_SERVER_PASSWORD,
             "database": process.env.DB_SERVER_DATABASE,
