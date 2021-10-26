9f988468d0c76f26906fcf04f21a4b5147590ed4
abeebe-csumb <abeebe@csumb.edu>
2021-10-25 18:18:49 -0700

    install mysql dependencies and DB constants

--- app.js
+++ app.js
@@ -2,13 +2,26 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const pool = require("./dbPool.js");
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 //routes
-app.get('/', (req, res) => {
-    res.send('hello world');
-  }); //test
+app.get('/', async (req, res) => {
+    let rows = await executeSQL('select 1;', []);
+    res.send(rows);
+}); //test
+
+//functions
+async function executeSQL(sql, params) {
+    return new Promise(function (resolve, reject) {
+        pool.query(sql, params, function (err, rows, fields) {
+            if (err) throw err;
+            resolve(rows);
+        });
+    });
+}
 
 //listen
 app.listen(port, () => {
